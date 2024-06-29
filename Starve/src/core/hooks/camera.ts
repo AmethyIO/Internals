c157bb80d85fcb24f0c0259e51efa2fb4d6e9bca
norelock <norbert.podbieglyyy@gmail.com>
2024-06-29 21:38:10 +0200

    chore(starve/v2): remove useless things in camera hook

--- Starve/src/core/hooks/camera.ts
+++ Starve/src/core/hooks/camera.ts
@@ -3,8 +3,6 @@
 import { PROPS, VARS } from '@/core';
 import { globalObject } from '../utils';
 
-let oldx = 0;
-let oldy = 0;
 let camx = -1;
 let camy = -1;
 
@@ -14,26 +12,22 @@ export function getCameraPosition(): number[] {
 
 export function updateCameraPosition(): void {
   if (!VARS.USER[PROPS.ALIVE] || (VARS.CLIENT[PROPS.SOCKET] && VARS.CLIENT[PROPS.SOCKET]['readyState'] !== globalObject.WebSocket.OPEN)) {
-    if (camx !== 0 || camy !== 0 || oldx !== 0 || oldy !== 0)
-      camx = camy = oldx = oldy = 0;
+    if (camx !== 0 || camy !== 0)
+      camx = camy = 0;
 
-    // console.log('update cam (Not alive)');
     return;
   }
 
   for (const prop1 in VARS.USER) {
     for (const prop2 in VARS.USER[prop1]) {
       switch (prop2) {
-        case "x":
+        case 'x':
           camx = VARS.USER[prop1][prop2];
           break;
-        case "y":
+        case 'y':
           camy = VARS.USER[prop1][prop2];
           break;
       }
     }
   }
-
-  oldx = camx;
-  oldy = camy;
 }
